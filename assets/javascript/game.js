23b3130a87824d40c8677bd6abfed3f5cfdb1eca
kimmybeee <kimariehavertybozman@gmail.com>
2018-01-20 13:48:00 -0800

    html written

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+alert("Welcome to Crystal Collector!");
+alert("Click crystals to add to your Score.");
+alert("The value of each crystal changes from game to game. It's a surprise!");
+alert("Don't let your Score exceed your Goal!");
+alert("Goooood Luck!!!");
+
