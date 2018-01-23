c2ac394d313cd10965cb52abe47855266e66d4ea
kimmybeee <kimariehavertybozman@gmail.com>
2018-01-22 19:26:56 -0800

    final version (for now)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,3 @@
-// alert("Welcome to Crystal Collector!");
-// alert("Click crystals to add to your Score.");
-// alert("The value of each crystal changes from game to game. It's a surprise!");
-// alert("Don't let your Score exceed your Goal!");
-// alert("Goooood Luck!!!");
-
 
 // Variables
 var targetScore = 0;
@@ -11,39 +5,52 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 
+//Places a zero next to wins and losses in the record box.
 $("#wins").text("0");
 $("#losses").text("0");
 
 //Reset all the values in the game.
     function resetGame() {
-    	score = 0;
-    	$('#score').text("0");
-	  	$('#crysone').attr("data-score", Math.floor((Math.random() * 11) + 2));
-	  	$('#crystwo').attr("data-score", Math.floor((Math.random() * 11) + 2));
-	  	$('#crysthree').attr("data-score", Math.floor((Math.random() * 11) + 2));
-	  	$('#crysfour').attr('data-score', Math.floor((Math.random() * 11) + 2));
 
+//Reset the score to zero.
+    	score = 0;
+    	$("#score").text("0");
+
+//Generates new random values for the crystals.
+	  	$("#crysone").attr("data-score", Math.floor((Math.random() * 11) + 2));
+	  	$("#crystwo").attr("data-score", Math.floor((Math.random() * 11) + 2));
+	  	$("#crysthree").attr("data-score", Math.floor((Math.random() * 11) + 2));
+	  	$("#crysfour").attr("data-score", Math.floor((Math.random() * 11) + 2));
+
+//Generates new random value for the goal score.
 	  	targetScore = Math.floor((Math.random() * 101) + 19);
 	  	$("#goal").text(targetScore);
     };
 
-	$('.crystal').on('click', function(){
-		score += parseInt($(this).attr('data-score'));
-		$('#score').text(score)
+//Adds the value of the clicked crystal to the score
+	$(".crystal").on('click', function(){
+		score += parseInt($(this).attr("data-score"));
+		$('#score').text(score);
 
+//Sets up the win scenario, adds to win total and triggers reset when goal is matched by score
 		if (score === targetScore){
 			wins++;
 			$("#wins").text(wins);
-			alert("You Winnin'")
+			alert("Yaay! You won!");
 			resetGame();
+
+//Sets up the lose scenario, adds to losses total and triggers reset when score increases past goal
 		} else if (score > targetScore) {
 			losses++;
 			$("#losses").text(losses);
-			alert("You Losin'");
+			alert("Sorry! Better luck next time!");
 			resetGame();
 		}
 		console.log(score);
 	})
-
+//Calls on the reset function.
 	resetGame();
+
+
+//Notes: make it so all crystals can't be even
 
